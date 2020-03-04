1d8276cdf61c0f852b811aeee653e29ae34cc367
chad svastisalee <swasgrafx@gmail.com>
2020-03-03 21:25:40 -0600

    Import session and passport packages

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+var passport = require('passport');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var apiUsersRouter = require('./routes/api/users');
