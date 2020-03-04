7ce7bfbfabfca00c3a48c0fdd66d83c9fddc264b
chad svastisalee <swasgrafx@gmail.com>
2020-03-03 21:56:09 -0600

    Add an endpoint for user authentication

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+var passport = require('passport');
+
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
