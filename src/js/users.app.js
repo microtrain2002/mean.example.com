eb0bb9fbc7f1e539b64f3d442ba37e9ab60b3a0b
chad svastisalee <swasgrafx@gmail.com>
2020-03-05 20:41:09 -0600

    Pull a list of users from the API

--- src/js/users.app.js
+++ src/js/users.app.js
@@ -1,9 +1,29 @@
 var usersApp = (function() {
 
+    function viewUsers(){
+
+        let uri = `${window.location.origin}/api/users`;
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', uri);
+      
+        xhr.setRequestHeader(
+          'Content-Type',
+          'application/json; charset=UTF-8'
+        );
+      
+        xhr.send();
+      
+        xhr.onload = function(){
+          let data = JSON.parse(xhr.response);
+          console.log(data);
+        }
+      }
+      
     return {
       load: function(){
-        alert('LOADED');
-      }
+        // alert('LOADED');
+        viewUsers();
+        }
     }
   
   })();
