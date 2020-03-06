d8f09dd42d428c92828321e7b286c9c550ebd3c4
chad svastisalee <swasgrafx@gmail.com>
2020-03-05 19:22:20 -0600

    Add logout methods

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -62,13 +62,12 @@ router.post('/login', function(req, res, next) {
   });
 
   router.delete('/logout', function(req, res){
-    req.session.destroy(function (err) {
-      if(err){
-        return res.json({success: 'false'});
-      }else{
-        return res.json({success: 'true'});
-      }
-    });
+    req.logout();
+    if(!req.session.passport.user){
+      return res.json({success: 'true'});
+    }else{
+      return res.json({success: 'false'});
+    }
   });
-  
+    
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  console.log(req.session);
   res.render('index', { title: 'Express' });
+});
+
+router.get('/logout', function(req, res){
+  console.log(req.session);
+  req.logout();
+  console.log(req.session);
 });
 
 module.exports = router;
