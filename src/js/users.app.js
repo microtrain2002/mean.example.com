85447a5498a1959855f0ed18a158b25ed3d87845
chad svastisalee <swasgrafx@gmail.com>
2020-03-10 18:59:01 -0500

    Complete Users API

--- src/js/users.app.js
+++ src/js/users.app.js
@@ -280,8 +280,8 @@ var usersApp = (function() {
   
           <div class="text-center">
             <br>
-            <a class="btn btn-lg btn-danger text-white">
-              Yes delete ${data.user.username}
+            <a onclick="usersApp.deleteUser('${data.user._id}');" class="btn btn-lg btn-danger text-white">
+            Yes delete ${data.user.username}
             </a>
           </div>
   
@@ -290,6 +290,31 @@ var usersApp = (function() {
   
       app.innerHTML = card;
     }
+  }
+  
+  function deleteUser(id){
+
+    let uri = `${window.location.origin}/api/users/${id}`;
+    let xhr = new XMLHttpRequest();
+    xhr.open('DELETE', uri);
+  
+    xhr.setRequestHeader(
+      'Content-Type',
+      'application/json; charset=UTF-8'
+    );
+  
+    xhr.send();
+  
+    xhr.onload = function(){
+      let data = JSON.parse(xhr.response);
+      if(data.success === true){
+        window.location.hash = '#';
+      }else{
+        alert('Unknown error, the user could not be deleted');
+      }
+  
+    }
+  
   }
   
   return {
@@ -319,8 +344,12 @@ var usersApp = (function() {
           viewUsers();
           break;
       }
+    },
+
+    deleteUser: function(id){
+      deleteUser(id);
     }
-  }
+      }
 
 })();
 
